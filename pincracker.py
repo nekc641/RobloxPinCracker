ad0c985912b7684b750919b58e5849a8d6770ec6
nekc641 <saekoyarezu09@gmail.com>
2024-11-13 17:39:53 +0800

    Update pincracker.py

--- pincracker.py
+++ pincracker.py
@@ -8,8 +8,8 @@ import json
 try:
     import config
 except ImportError as e:
-    if path.isfile('config.py'):
     if path.isfile('config-template.py'):
+    if path.isfile('config.py'):
         raise
     else:
         raise ImportError('[FAILURE] The config.py file could not be found. Make sure you have made a config.py file using config-template.py.')
